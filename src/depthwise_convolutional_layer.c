31740d3b0642d5970a1ac4521027149f7a425f43
RJzz <jieruiruir@gmail.com>
2018-12-10 16:57:20 +0800

    fuck

--- src/depthwise_convolutional_layer.c
+++ src/depthwise_convolutional_layer.c
@@ -326,7 +326,7 @@ void backward_depthwise_convolutional_layer(depthwise_convolutional_layer l, net
             float *im = net.input + c * l.h * l.w + b * l.c * l.h * l.w;
 
             im2col_cpu(im, 1, l.n, l.w, l.size, l.stride, l.pad, boffset);
-            gemm(0, 1, 1, n, k, 1, aoffset, k, boffset, k, 1, coffet, n)；
+            gemm(0, 1, 1, n, k, 1, aoffset, k, boffset, k, 1, coffset, n)；
 
             if(net.delta) {
                 aoffset = l.weights + c * l.size * l.size;
